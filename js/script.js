8bb1b49d36b7b4369b02df74d90262c8e5f442b1
brennan-lee <113056160+brennan-lee@users.noreply.github.com>
2022-12-14 18:05:24 -0500

    js

--- js/script.js
+++ js/script.js
@@ -16,8 +16,34 @@ if (navigator.serviceWorker) {
 }
 
 /**
- * This function displays an alert.
- */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+ * Get API info.
+*/
+// code from: https://www.youtube.com/watch?v=670f71LTWpM
+
+const getImage = async (URLAddress) => {
+  try {
+    const result = await fetch(URLAddress)
+    const jsonData = await result.json()
+    console.log(jsonData)
+    document.getElementById("api-image").innerHTML =
+    '<img src="' + 
+      jsonData.url + 
+      '" alt="API image" class="center" ' +
+      '>'
+    if (jsonData.artist_url != "none") {
+      document.getElementById("image-artist").innerHTML =
+      "<p>Artist: " +
+      '<a href="' +      
+      jsonData.artist_url +
+      '">' +
+      jsonData.artist +
+      "</a>"
+  } else {
+    document.getElementById("image-artist").innerHTML = "<p>Artist: unknown</p>"
+  }
+  } catch (err) {
+    console.log(err)
+  }
 }
+
+getImage("https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5/img")
